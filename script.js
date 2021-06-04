6a8b48e2ff5db8ba02287a7d1d6c4dced6beec59
Stavitskiy Kostia <example@gmail.com>
2021-06-04 17:37:25 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//var number = 5;
+//var string = "Hello";
+//var sym = Symbol();
+//var boolean = true;
+//var obj = {} ;
+//console.log(4/0);
+//console.log('string' * 9);
+
+/*let persone = {
+    name: "Jone",
+    age:25,
+    isMarried: false
+};*/
+
+//console.log(persone.age);
+//let arr = ['plum.png' , "orangr.jpg" , "apple.bmp"];
+//let answer = confirm("How many age");
+//console.log(answer);
+//let answer = prompt("Есть ли вам 18?" , "ДА");
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+
+let isCheked = true,
+    isClouse = false;
+console,log(!isCheked || isClouse);
